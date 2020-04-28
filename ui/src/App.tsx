a2cd5c8666283f587e661faebcbc9ee9151f520f
Matej Sima <matej.sima@gmail.com>
2020-04-28 17:26:50 +0200

    cleanup ui

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
-import './config';
-import { Notary } from "./containers/Notary";
 
 class App extends React.Component {
 
   render() {
-    return <Notary/>
+    return <h1>Stove Labs' Notary</h1>
   }
 }
 
