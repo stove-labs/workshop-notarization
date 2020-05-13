91a0e6d89e9cb950b4809e482e157d589edc2aa1
Istvan Deak <dexterslabor@gmail.com>
2020-05-13 11:44:39 +0200

    Add contract error messages

--- sdk/helpers/constants.ts
+++ sdk/helpers/constants.ts
@@ -1,7 +1,16 @@
 export module constants {
     export const rpcErrors = {
         michelson: {
-            scriptRejected: "proto.006-PsCARTHA.michelson_v1.script_rejected"
+            scriptRejected: 'proto.006-PsCARTHA.michelson_v1.script_rejected',
+            runtimeError: 'proto.006-PsCARTHA.michelson_v1.runtime_error'
         }
     }
+    export const contractErrors = {
+        documentHashAlreadyNotarized: '1',
+        documentNotFound: '2',
+        documentAlreadySigned: '3',
+        senderIsNotASigneeForThisDocument: '4'
+    }
 }
+
+
